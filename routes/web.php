34c6448ba10a729cf8d38b3191278cdc56ebd2fd
JokiBalap <proscandra7@gmail.com>
2026-06-11 17:03:21 +0800

    fix: allow GET /logout to avoid 419 Page Expired error

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
 // Main App Dashboard View
 Route::get('/', function () {
