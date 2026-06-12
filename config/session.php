9b1263908f079488c5760f85c0d72e39bc244606
JokiBalap <proscandra7@gmail.com>
2026-06-12 11:28:39 +0800

    Fix: Resolve 419 Page Expired errors by enabling dynamic secure session cookies for ngrok tunnels

--- config/session.php
+++ config/session.php
@@ -168,7 +168,11 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', (
+        (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ||
+        (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ||
+        (isset($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] === 'on')
+    )),
 
     /*
     |--------------------------------------------------------------------------
